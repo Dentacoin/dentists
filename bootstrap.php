871a943cc21e713e6ea0d39328d112e56cf754d6
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-08 13:59:38 +0300

    add BASE_URL param

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,7 @@ define('DS', '/');
 // define base directory for project
 define('ROOT', realpath(dirname(__FILE__)) . DS);
 // define uploads directory
+define('BASE_URL', 'https://dentists.dentacoin.com/');
 define('UPLOADS', ROOT . 'public' . DS . 'assets' . DS . 'uploads' . DS);
 define('UPLOADS_FRONT_END', DS . 'assets' . DS . 'uploads' . DS);
 define('MAX_UPL_SIZE', 2097152);
