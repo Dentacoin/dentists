ca052218616a9aa3bcab164dfc93832bf58daef8
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-08 15:08:02 +0300

    test sendgrid

--- config/mail.php
+++ config/mail.php
@@ -4,6 +4,12 @@ return array(
     'driver' => 'smtp',
     'host' => 'smtp.sendgrid.net',
     'port' => 587,
+    'from' => array(
+        'address' => 'reviews@dentacoin.com',
+        'name' => 'Dentacoin Trusted Review Platform',
+        'address-vox' => 'dentavox@dentacoin.com',
+        'name-vox' => 'DentaVox Market Research Platform',
+    ),
     'encryption' => 'tls',
     'username' => env('SENDGRID_USERNAME'),
     'password' => env('SENDGRID_PASSWORD'),
