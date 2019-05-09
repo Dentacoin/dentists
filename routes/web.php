3ede48e8280fed768d5ec08d18dd323556407f11
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-09 14:50:16 +0300

    test custom cookie

--- routes/web.php
+++ routes/web.php
@@ -29,18 +29,6 @@ Route::group(['prefix' => '/', 'middleware' => 'frontEndMiddleware'], function (
 
     Route::get('/home', 'HomeController@getNotLoggedView')->middleware('HandleUserSession')->name('logged-home');
 
-    Route::get('/test', function() {
-        $encrypted = (new \App\Http\Controllers\Controller())->encrypt(session('logged_user')['id'], getenv('API_ENCRYPTION_METHOD'), getenv('API_ENCRYPTION_KEY'));
-        $url = urlencode($encrypted);
-        echo '<br>';
-        var_dump($url);
-        echo '<br>';
-        var_dump(urldecode($url));
-        echo '<br>';
-        var_dump((new \App\Http\Controllers\Controller())->decrypt($encrypted));
-        die('asd');
-    });
-
     Route::get('/my-profile', 'UserController@getMyProfileView')->middleware('HandleUserSession')->name('my-profile');
 
     Route::get('/edit-account', 'UserController@getEditAccountView')->middleware('HandleUserSession')->name('edit-account');
