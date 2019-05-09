09b1b56b40ecb46654f9602e1a759d8c3abdb773
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-09 15:17:00 +0300

    test custom cookie

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -521,7 +521,7 @@
     @else
         @php($slug = (new \App\Http\Controllers\Controller())->encrypt(session('logged_user')['id'], getenv('API_ENCRYPTION_METHOD'), getenv('API_ENCRYPTION_KEY')))
         @php($type = (new \App\Http\Controllers\Controller())->encrypt(session('logged_user')['type'], getenv('API_ENCRYPTION_METHOD'), getenv('API_ENCRYPTION_KEY')))
-        <img src="//dentacoin.com/custom-cookie?slug={{ urlencode($encrypted) }}&type={{ urlencode($type) }}" class="hide"/>
+        <img src="//dentacoin.com/custom-cookie?slug={{ urlencode($slug) }}&type={{ urlencode($type) }}" class="hide"/>
     @endif
     <div class="bottom-fixed-container">
         @if(!empty($privacy_policy_cookie))
