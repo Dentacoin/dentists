1f7cb85bf92ae02682efa9714c754f1b857d9384
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-16 08:49:49 +0300

    change the version of resouces

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -584,16 +584,16 @@
     </div>
     {{--<script src="/assets/js/basic.js"></script>--}}
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaVeHq_LOhQndssbmw-aDnlMwUG73yCdk&libraries=places&language=en"></script>
-    <script src="/dist/js/front-libs-script.js?v=1.0.24"></script>
+    <script src="/dist/js/front-libs-script.js?v=1.0.25"></script>
     {{--<script src="/assets/js/address.js"></script>--}}
     @yield("script_block")
-    <script src="/dist/js/front-script.js?v=1.0.24"></script>
-    <script src="/assets/js/index-bundled.js?v=1.0.24"></script>
+    <script src="/dist/js/front-script.js?v=1.0.25"></script>
+    <script src="/assets/js/index-bundled.js?v=1.0.25"></script>
 
     {{--Load social logging scripts only if user is not logged--}}
     @if(!(new \App\Http\Controllers\UserController())->checkSession())
-        <script src="//dentacoin.com/assets/libs/civic-login/civic.js?v=1.0.24"></script>
-        <script src="//dentacoin.com/assets/libs/facebook-login/facebook.js?v=1.0.24"></script>
+        <script src="//dentacoin.com/assets/libs/civic-login/civic.js?v=1.0.25"></script>
+        <script src="//dentacoin.com/assets/libs/facebook-login/facebook.js?v=1.0.25"></script>
     @endif
 
     {{--Multiple errors from laravel validation--}}
