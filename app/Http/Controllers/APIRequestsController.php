2d3ba5d84c93a5bcc2f382dfadc126347c0f7435
unknown <miroslav.nedelchev@dentacoin.com>
2019-06-21 11:50:48 +0300

    test register result

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -62,7 +62,6 @@ class APIRequestsController extends Controller {
         }
 
         var_dump($post_fields_arr);
-        die('asd');
 
         $curl = curl_init();
         curl_setopt_array($curl, array(
