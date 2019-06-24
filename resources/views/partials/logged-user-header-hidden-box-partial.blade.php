5cebc5d4d3ad7365dfb32fc0af59dabba07b8ebf
unknown <miroslav.nedelchev@dentacoin.com>
2019-06-24 13:40:02 +0300

    change style of my profile btn

--- resources/views/partials/logged-user-header-hidden-box-partial.blade.php
+++ resources/views/partials/logged-user-header-hidden-box-partial.blade.php
@@ -38,7 +38,7 @@
                 </div>
                 <div class="col-xs-6 inline-block">
                     {{--<a href="{{ route('my-profile') }}" class="fs-16 white-dark-blue-btn">My account</a>--}}
-                    <a href="//account.dentacoin.com?platform=dentacoin" class="fs-16 white-dark-blue-btn">My account</a>
+                    <a href="//account.dentacoin.com?platform=dentists" class="fs-16 white-dark-blue-btn">My account</a>
                 </div>
             </div>
         </div>
