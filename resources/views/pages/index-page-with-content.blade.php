4d2d336ad6f6bfa31ea61285f020ffdddfdcbd1c
unknown <miroslav.nedelchev@dentacoin.com>
2019-07-10 08:06:23 +0300

    stop maintenance mood

--- resources/views/pages/index-page-with-content.blade.php
+++ resources/views/pages/index-page-with-content.blade.php
@@ -1,7 +1,7 @@
 @extends("layout")
 @section("content")
-    {{--{{(new \App\Http\Controllers\PagesController())->shortcodeExtractor(html_entity_decode($page->html))}}--}}
-    <div class="faq-container">
+    {{(new \App\Http\Controllers\PagesController())->shortcodeExtractor(html_entity_decode($page->html))}}
+    {{--<div class="faq-container">
         <section class="title container">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
@@ -86,5 +86,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>--}}
 @endsection
