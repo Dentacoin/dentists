a07224de76c039bd07099c2f2e1069da3cea8b6c
unknown <miroslav.nedelchev@dentacoin.com>
2019-08-14 17:44:20 +0300

    replace coingecko reading api with coinmarketcap

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -48,6 +48,29 @@ class Controller extends BaseController
         return Page::where(array('slug' => $route))->get()->first();
     }
 
+    public function getCurrentDcnUsdRate()  {
+        //API connection
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://api.coinmarketcap.com/v1/ticker/dentacoin/?convert=USD',
+            CURLOPT_SSL_VERIFYPEER => 0
+        ));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        $resp = json_decode(curl_exec($curl));
+        curl_close($curl);
+
+        if(sizeof($resp) > 0)   {
+            if(property_exists($resp[0], 'price_usd'))  {
+                return (float)$resp[0]->price_usd;
+            }else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
+
     public function getMenu($menu_slug) {
         return MenuElement::where(array('menu_id' => Menu::where(array('slug' => $menu_slug))->get()->first()->id))->get()->sortBy('order_id');
     }
--- resources/views/partials/logged-user-header-hidden-box-partial.blade.php
+++ resources/views/partials/logged-user-header-hidden-box-partial.blade.php
@@ -1,6 +1,11 @@
 <div class="hidden-box-parent">
     @php($user_data = (new \App\Http\Controllers\APIRequestsController())->getUserData(session('logged_user')['id']))
-    <span class="fs-14 padding-right-10 user-name">{{$user_data->name}}</span>
+    <div class="inline-block fs-14 padding-right-10 color-white-on-hub">
+        <span class="user-name">{{$user_data->name}}</span>
+        @php($dcn_balance = (new \App\Http\Controllers\APIRequestsController())->getDCNBalance()->data)
+        @php($usd_balance = (new \App\Http\Controllers\Controller())->getCurrentDcnUsdRate() * $dcn_balance)
+        <div>{{$dcn_balance}} DCN | ${{number_format((float)$usd_balance, 2, '.', '')}}</div>
+    </div>
     <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block header-avatar">
         @if(!empty($user_data->thumbnail_url))
             <img alt="" itemprop="contentUrl" src="{{$user_data->thumbnail_url}}"/>
