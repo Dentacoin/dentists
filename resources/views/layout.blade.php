6c0163e43ba19e0405001d0706a81efb07be04b2
unknown <miroslav.nedelchev@dentacoin.com>
2019-12-03 08:40:04 +0200

    test fb login

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -30,14 +30,14 @@
     <script>
         var HOME_URL = '{{ route("home") }}';
     </script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
+    {{--<!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-97167262-3"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'UA-97167262-3');
-    </script>
+    </script>--}}
 
     <!-- Facebook Pixel Code -->
     <script>
