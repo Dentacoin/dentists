89620a5939ba8837a158736d987b593f417907c4
unknown <miroslav.nedelchev@dentacoin.com>
2020-07-20 11:44:53 +0300

    test facebook login and facebook cat

--- resources/views/layout-redesign.blade.php
+++ resources/views/layout-redesign.blade.php
@@ -131,9 +131,9 @@
             </figure>
             @if(!(new \App\Http\Controllers\UserController())->checkSession())
                 <div class="col-xs-9 inline-block btns-container padding-right-xs-10">
-                    <a href="/how-it-works" class="hide-xs fs-18 color-black calibri-bold inline-block padding-left-10 padding-right-10 hide-on-scroll">How it works</a>
-                    <span class="hide-xs inline-block padding-left-10 padding-right-10 fs-18 color-black hide-on-scroll">|</span>
-                    <a @if (Route::current()->getName() != 'home-redesign' && Route::current()->getName() != 'how-it-works') href="{{route('home-redesign')}}#contact-us" @else href="javascript:void(0);" id="contact-us" @endif class="hide-xs fs-18 color-black calibri-bold inline-block padding-left-10 padding-right-10 @if (Route::current()->getName() == 'home-redesign' || Route::current()->getName() == 'how-it-works') scrolling-to-section @endif hide-on-scroll">Contact us</a>
+                    <a href="/how-it-works" class="hide-xs fs-18 color-black calibri-bold inline-block padding-left-10 padding-right-10">How it works</a>
+                    <span class="hide-xs inline-block padding-left-10 padding-right-10 fs-18 color-black">|</span>
+                    <a @if (Route::current()->getName() != 'home-redesign' && Route::current()->getName() != 'how-it-works') href="{{route('home-redesign')}}#contact-us" @else href="javascript:void(0);" id="contact-us" @endif class="hide-xs fs-18 color-black calibri-bold inline-block padding-left-10 padding-right-10 @if (Route::current()->getName() == 'home-redesign' || Route::current()->getName() == 'how-it-works') scrolling-to-section @endif">Contact us</a>
                     <a href="javascript:void(0)" class="white-dark-blue-btn open-dentacoin-gateway dentist-register inline-block margin-left-15" tabindex="-1">SIGN UP</a>
                     <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block margin-left-15 hamburger-icon">
                         <a href="javascript:void(0)" class="hamburger display-block">
